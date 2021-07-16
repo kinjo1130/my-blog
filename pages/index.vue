c69330a0a611b9412ccbfceb7c83aa79c6ac708e
AFURO-1130 <softtennis6161130@icloud.com>
2021-07-16 16:58:27 +0900

    www

--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,11 @@
 </template>
 
 <script>
-export default {}
+export default {
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
+};
 </script>
