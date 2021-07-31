9e8e63a6dcd89b0c1ec4827d2739a80d5bc8ea8e
AFURO-1130 <73173075+AFURO-1130@users.noreply.github.com>
2021-07-31 14:47:00 +0900

    favicon変更

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
   <div class="max-w-screen-2xl container mx-auto">
     <div class="mx-10 sm:mx-10 md:mx-10 lg:mx-48">
       <!-- ヘッダー -->
-      <div class="border-b-2 py-5">
-        <nuxt-link to="/" class="text-center sm:text-left text-2xl"
+      <div class="border-b-2 py-5 text-center sm:text-left">
+        <nuxt-link to="/" class="text-center sm:text-center text-2xl"
           >Blog</nuxt-link
         >
       </div>
