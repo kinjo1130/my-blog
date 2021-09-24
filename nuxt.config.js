4d410b116feeb25cc56d4df473abe4423e1894fe
AFURO-1130 <73173075+AFURO-1130@users.noreply.github.com>
2021-09-24 19:56:58 +0900

    faviconの設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
       { name: "google-site-verification", content: "PtYzxYyQoXOUhMNBGYRzN56uPOUCSR5_6dFkZ22v0I4" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: 'img/favicon.ico' }
     ]
   },
 
